237c7e09714d21ba73b6e11b8375dd0f547629c1
Lucas Martins <lucaslogan72@gmail.com>
2022-06-16 09:08:13 -0300

    Add: Modulo de abas das cautelas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AbasModule } from './abas/abas.module'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AbasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
